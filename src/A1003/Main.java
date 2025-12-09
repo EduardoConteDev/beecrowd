49cacaa3f53358de9aa1e212f784bd26ac1a5a1d
Eduardo Conte <eduardo.conte.dev@gmail.com>
2025-12-08 22:10:13 -0300

    1003

---
+++ src/A1003/Main.java
@@ -0,0 +1,16 @@
+package A1003;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("SOMA = " + soma);
+    }
+}
